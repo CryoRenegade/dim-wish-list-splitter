5d8cf30c35713bf7c6f0a424681cfe33ff7a5c7b
2Pillows <25855654+2Pillows@users.noreply.github.com>
2022-08-03 22:57:46 -0400

    cleaned up .py and adjusted credit filters

--- voltron_splitter.py
+++ voltron_splitter.py
@@ -1,130 +1,120 @@
 # git submodule update --remote
 
-#voltron file in submodule
-mainFile = "./dim-wish-list-sources/voltron.txt"
+# read lines of file and trip flags
+class FileInfo:
+    def __init__(self, allLists):
+        self.pveFlag = False
+        self.pvpFlag = False
+        self.mkbFlag = False
+        self.ctrFlag = False
 
-pveFlag = False
-pvpFlag = False
-mkbFlag = False
-ctrFlag = False
-dimFlag = False
+        self.dimFlag = False
+        self.creditFlag = False
 
-listSettings = [
-                {
-                "flags": "pveFlag or not pvpFlag", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvE.txt"
-                },
-                {
-                "flags": "pvpFlag", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvP.txt"
-                },
-                {
-                "flags": "mkbFlag or not ctrFlag", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/MKB.txt"
-                },
-                {
-                "flags": "ctrFlag", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/Controller.txt"
-                },
-                {
-                "flags": "(pveFlag or not pvpFlag) and (mkbFlag or not ctrFlag)", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvE-MKB.txt"
-                },
-                {
-                "flags": "(pveFlag or not pvpFlag) and ctrFlag", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvE-Controller.txt"
-                },
-                {
-                "flags": "pvpFlag and (mkbFlag or not ctrFlag)", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvP-MKB.txt"
-                },
-                {
-                "flags": "pvpFlag and ctrFlag", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvP-Controller.txt"
-                },
-                {
-                "flags": "pveFlag and pvpFlag", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvE-PvP.txt"
-                },
-                {
-                "flags": "(pveFlag and pvpFlag) and (mkbFlag or not ctrFlag)", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvE-PvP-MKB.txt"
-                },
-                {
-                "flags": "(pveFlag and pvpFlag) and ctrFlag", 
-                "search":[], "searchFlag": False,
-                "FileName":"./wishlists/PvE-PvP-Controller.txt"
-                },
-                {
-                "flags": "False", 
-                "search":["pandapaxxy"], "searchFlag": False,
-                "FileName":"./wishlists/PandaPaxxy.txt"
-                }
-                ]
+        self.allLists = allLists
 
-lineCollection = []
+        self.mainFile = "./dim-wish-list-sources/voltron.txt"
 
-#function to open and clean files
-def clearFiles(wishListAll):
-    for curList in wishListAll:
-        with open(curList["FileName"], mode='w') as clearFile:
-            pass
-
-def checkWrite():
-    for listParams in listSettings:
-        if lineCollection != []:
-            if (eval(listParams["flags"]) or listParams["searchFlag"]) or not dimFlag:
-                with open(listParams["FileName"], mode='a') as tempFile:
-                    for i in lineCollection:
-                        tempFile.write(i)
-                    tempFile.write("\n")
-
-        listParams["searchFlag"] = False
-
-clearFiles(listSettings)
-
-with open(mainFile, mode='r', encoding='utf-8') as f:
-    for line in f:
-        #Non-empty Line. Add to Weapon. Check for Flags
-        if len(line) != 1:
-            lineCollection.append(line)
-            
-            if 'pve' in line.lower():
-                pveFlag = True
-            if 'pvp' in line.lower():
-                pvpFlag = True
-            if 'mkb' in line.lower() or "m+kb" in line.lower():
-                mkbFlag = True
-            if 'controller' in line.lower():
-                ctrFlag = True
-            if 'dimwishlist:item' in line.lower():
-                dimFlag = True
-            
-            for listParams in listSettings:
-                if any(i in line.lower() for i in listParams["search"]):
-                    listParams["searchFlag"] = True
-                    
-        #Empty Line. Add Weapon to WishLists. Reset Flags
-        else:
-            checkWrite()
-            
-            pveFlag = False
-            pvpFlag = False
-            mkbFlag = False
-            ctrFlag = False
-            dimFlag = False
-            
-            lineCollection = []
+        self.lineCollection = []
         
-     
-    checkWrite()
+        self.clearFiles()
+        self.readMain()
+
+    def readMain(self):
+        with open(self.mainFile, mode='r', encoding='utf-8') as f:
+            for line in f:
+                # Non-empty Line
+                if len(line) != 1:
+                    self.lineCollection.append(line)
+
+                    if 'pve' in line.lower():
+                        self.pveFlag = True
+                    if 'pvp' in line.lower():
+                        self.pvpFlag = True
+                    if 'mkb' in line.lower() or "m+kb" in line.lower():
+                        self.mkbFlag = True
+                    if 'controller' in line.lower():
+                        self.ctrFlag = True
+                    if 'dimwishlist:item' in line.lower():
+                        self.dimFlag = True
+                    if 'https://' in line.lower():
+                        self.creditFlag = True
+
+                    for listSettings in self.allLists:
+                        if any(i in line.lower() for i in listSettings.getSearch()):
+                            listSettings.setSearchFlag(True)
+
+                # Empty Line
+                else:
+                    self.checkWrite()
+
+                    self.pveFlag = False
+                    self.pvpFlag = False
+                    self.mkbFlag = False
+                    self.ctrFlag = False
+                    self.dimFlag = False
+                    self.creditFlag = False
+                    
+                    self.lineCollection = []
+        
+        self.checkWrite()
+
+    def clearFiles(self):
+        for curList in self.allLists:
+            with open(curList.getFileName(), mode='w') as clearFile:
+                pass
+
+    def checkWrite(self):
+        for curList in self.allLists:
+            if self.lineCollection != []:
+                if (eval(curList.getFlags()) or curList.getSearchFlag()) or (not self.dimFlag and self.creditFlag):
+                    with open(curList.getFileName(), mode='a') as tempFile:
+                        for i in self.lineCollection:
+                            tempFile.write(i)
+                        tempFile.write("\n")
+
+            curList.setSearchFlag(False)
+class ListSetting:
+    def __init__(self, flags, search, fileName):
+        self.flags = flags
+        self.search = search
+        self.fileName = fileName
+
+        self.searchFlag = False
+
+    def getFlags(self):
+        return self.flags
+    def getSearch(self):
+        return self.search
+    def getSearchFlag(self):
+        return self.searchFlag
+    def getFileName(self):
+        return self.fileName
+
+    def setSearchFlag(self, newFlag):
+        self.searchFlag = newFlag
+    
+
+wishLists = []
+
+# create individual lists and add to wishLists
+wishLists.append(ListSetting("self.pveFlag or not self.pvpFlag", [], "./wishlists/PvE.txt"))
+wishLists.append(ListSetting("self.pvpFlag", [], "./wishlists/PvP.txt"))
+
+wishLists.append(ListSetting("self.mkbFlag or not self.ctrFlag", [], "./wishlists/MKB.txt"))
+wishLists.append(ListSetting("self.ctrFlag", [], "./wishlists/Controller.txt"))
+
+wishLists.append(ListSetting("(self.pveFlag or not self.pvpFlag) and (self.mkbFlag or not self.ctrFlag)", [], "./wishlists/PvE-MKB.txt"))
+wishLists.append(ListSetting("(self.pveFlag or not self.pvpFlag) and self.ctrFlag", [], "./wishlists/PvE-Controller.txt"))
+
+wishLists.append(ListSetting("self.pvpFlag and (self.mkbFlag or not self.ctrFlag)", [], "./wishlists/PvP-MKB.txt"))
+wishLists.append(ListSetting("self.pvpFlag and self.ctrFlag", [], "./wishlists/PvP-Controller.txt"))
+
+wishLists.append(ListSetting("self.pveFlag and self.pvpFlag", [], "./wishlists/PvE-PvP.txt"))
+
+wishLists.append(ListSetting("(self.pveFlag and self.pvpFlag) and (self.mkbFlag or not self.ctrFlag)", [], "./wishlists/PvE-PvP-MKB.txt"))
+wishLists.append(ListSetting("(self.pveFlag and self.pvpFlag) and self.ctrFlag", [], "./wishlists/PvE-PvP-Controller.txt"))
+
+wishLists.append(ListSetting("False", ["pandapaxxy"], "./wishlists/PandaPaxxy.txt"))
+
+FileInfo(wishLists)
